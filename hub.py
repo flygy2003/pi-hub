57c51980ef9e5bfa089c0cb67f7f8232ce498116
flygy2003 <flygy2003@gmail.com>
2017-06-27 10:30:05 +0000

    backup

--- hub.py
+++ hub.py
@@ -1,10 +1,25 @@
 import pyrebase as fb
+import RPi.GPIO as io
+import time
+
+io.setmode(io.BOARD)
+
+def blink(pin):  
+	io.output(pin,io.HIGH)  
+	time.sleep(2)
+	print("Slept first round w/ HIGH")  
+	io.output(pin,io.LOW)  
+	time.sleep(2)  
+	print("Slept second round w/ LOW")
+	io.cleanup()
+
+io.setup(32, io.OUT)
 
 config = {
-    "apiKey": "AIzaSyDJ31YrXt8JAPUZHYGNRS8WNjoHaz8ssuE",
-    "authDomain": "home-b7104.firebaseapp.com",
-    "databaseURL": "https://home-b7104.firebaseio.com",
-    "storageBucket": "home-b7104.appspot.com"
+	"apiKey": "AIzaSyDJ31YrXt8JAPUZHYGNRS8WNjoHaz8ssuE",
+	"authDomain": "home-b7104.firebaseapp.com",
+	"databaseURL": "https://home-b7104.firebaseio.com",
+	"storageBucket": "home-b7104.appspot.com"
 }
 
 firebase = fb.initialize_app(config)
@@ -12,3 +27,7 @@ db = firebase.database()
 
 rooms = db.child("rooms").get()
 print(rooms.val())
+print("------------------------------------------------------------")
+time.sleep(2)
+print("I slept")
+blink(32)
